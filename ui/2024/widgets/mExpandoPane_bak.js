2f0cdfc0636327ef6a9c3cb350c526f5aecf85a4
Mike Trivedi <106334350+TrivediEnterprisesInc@users.noreply.github.com>
2024-02-24 11:14:14 -0500

    Create mExpandoPane_bak.js

---
+++ ui/2024/widgets/mExpandoPane_bak.js
@@ -0,0 +1,22 @@
+require(["dojo/_base/lang", "dojox.layout.ExpandoPane", "dojo/dom", 
+        "dojo/dom-class", "dojo/text!../support/mExpandoPane.html", 
+        "dojo/domReady!"], function(lang, ExpandoPane, dom, domClass, template){
+
+   return declare(mExpandoPane, {
+      lang.extend(ExpandoPane, {
+         templateString: template,
+         postMixInProperties: function(evt){
+                        //alert('sel clicked 4: "' + this.title + '"');
+                        //nd to copy id for action >> state >> selItems >> clear
+                        if (this.expButton.innerHTML === "<<"){
+                           //this.expButton.innerHTML = "check_box_outline_blank"
+                           alert("<<");
+                        } else {
+                           //this.expButton.innerHTML = "check_box"
+                           alert(">>");
+                        };
+                        //evt.stopPropagation();
+                  }
+      })
+   })
+})
