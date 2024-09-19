6556398e9097d390eb89982773f8dc182e589781
Brij User <133048049+brijUser2@users.noreply.github.com>
2024-09-19 16:07:12 -0400

    Update FrmMngrExt.js

---
+++ ui/2024/form/gridx_Styling/FrmMngrExt.js
@@ -0,0 +1,38 @@
+require(["dojo/_base/lang", "dijit/registry", "dojox/form/Manager"], function(lang, registry, Mngr){
+  lang.extend(Mngr, {
+    frmDeltas:{},
+    addDelta: function(f, v){
+	//console.log("lang.extend.Mngr call 4 addDelta f=" + f + " v=" + v);
+	if (has(this.frmDeltas, f)) {
+		//console.log("addDelta: fld exists");
+		if (get(this.frmDeltas, f) === v){
+			//console.log("addDelta: same val exists");
+		} else {
+			//console.log("addDelta: new edit");
+			this.frmDeltas[f] = v;
+		}
+	} else {
+			//console.log("addDelta: fld doesn't exist: " + f);
+			this.frmDeltas[f] = v;
+	}
+    },
+    getDeltas: function(){
+	   console.log("frmDeltas: ", this.frmDeltas);
+    },
+    insp: function(r){
+        console.log("insp.formWidgets", Object.keys(this.formWidgets));
+        //this.setFormValues({bool: true, number:1011, bogus: "not exist", string:"Set in frmMngf.insp"});
+        this.setFormValues(r);
+        // collect all current values of attached nodes
+        //this.inspect(inspector);
+        
+    }
+
+/*,
+    postCreate: function() {
+        //apparently not necc (not in widgt) this.inherited();
+        console.log("lang.extend.Mngr postCreate call...");
+    }
+*/
+  });
+});
